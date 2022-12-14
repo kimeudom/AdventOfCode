91b7afb59b393398bd5c4bcd09c24b9194951375
dom <kimeudom.git@gmail.com>
2022-12-14 22:33:53 +0300

    minor cleanup, will be back soon

--- DAY9/1/soln.cpp
+++ DAY9/1/soln.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <set>
 #include <stdlib.h>
+#include <unistd.h>
 #include <utility>
 
 using namespace std;
@@ -55,30 +56,31 @@ bool isAdjacent(int tx, int ty, int hx, int hy) {
 }
 
 void moveTail(RopeEnd *h, RopeEnd *t, int tx, int ty, int hx, int hy) {
-  if (hx == tx) {
+  if (hy == ty) {
     // Horizontal movement
     (hx > tx) ? t->setX(tx + 1) : t->setX(tx - 1);
     return;
-
-  }
-  //  Vertical movement
-  else if (hy == ty) {
+  } else if (hx == tx) {
+    //  Vertical movement
     (hy > ty) ? t->setY(ty + 1) : t->setY(ty - 1);
     return;
   }
 
   //  Diagonal movement
-  bool UR = (hx > tx) && (hy > ty);
-  bool UL = (hx < tx) && (hy > ty);
-  bool LR = (hx > tx) && (hy < ty);
-  bool LL = (hx < tx) && (hy < ty);
+  bool UR = ((hx > tx) && (hy > ty));
+  bool UL = ((hx < tx) && (hy > ty));
+  bool LR = ((hx > tx) && (hy < ty));
+  bool LL = ((hx < tx) && (hy < ty));
 
   if (UR) {
     t->setX(tx + 1), t->setY(ty + 1);
+    return;
   } else if (UL) {
     t->setX(tx - 1), t->setY(ty + 1);
+    return;
   } else if (LR) {
     t->setX(tx + 1), t->setY(ty - 1);
+    return;
   } else if (LL) {
     t->setX(tx - 1), t->setY(ty - 1);
   }
@@ -100,6 +102,33 @@ void moveHead(RopeEnd *h, char dir) {
     h->setY(h->getY() - 1);
     break;
   };
+}
+
+void addNull(char arr[][36]) {
+  for (int i = 0; i < 36; i++) {
+    for (int j = 0; j < 36; j++) {
+      arr[i][j] = ' ';
+    }
+  }
+}
+
+void printArr(char arr[][36]) {
+  for (int i = 0; i < 36; i++) {
+    for (int j = 0; j < 36; j++) {
+      cout << "[" << arr[i][j] << "] ";
+    }
+    cout << endl;
+  }
+}
+
+void addToArr(char arr[][36], int x, int y, char c) {
+  if (c == 'H') {
+    if (arr[x + 17][y + 17] != '#') {
+      arr[x + 17][y + 17] = '*';
+    }
+  } else {
+    arr[x + 17][y + 17] = '#';
+  }
 }
 
 int main(int argc, char **argv) {
@@ -125,6 +154,10 @@ int main(int argc, char **argv) {
   // Add the origin directory
   s.insert({0, 0});
 
+  // Fill the arr
+  char arr[36][36];
+  addNull(arr);
+
   while (input.peek() != EOF) {
 
     getline(input, line, '\n');
@@ -133,16 +166,23 @@ int main(int argc, char **argv) {
     // cout << line << endl;
 
     for (int i = 0; i < counter; i++) {
+      system("clear");
       moveHead(h, dir);
+      addToArr(arr, h->getX(), h->getY(), 'H');
+      printArr(arr);
+      sleep(2);
       int tx, ty, hx, hy;
       tx = t->getX(), ty = t->getY(), hx = h->getX(), hy = h->getY();
 
       if (!isAdjacent(tx, ty, hx, hy)) {
+        system("clear");
         moveTail(h, t, tx, ty, hx, hy);
+        addToArr(arr, t->getX(), t->getY(), 'T');
+        printArr(arr);
+        sleep(2);
+
         s.insert({t->getX(), t->getY()});
       }
-      // cout << "(" << h->getX() << "," << h->getY() << ")";
-      // cout << "(" << t->getX() << "," << t->getY() << ")" << endl;
     }
   }
 
